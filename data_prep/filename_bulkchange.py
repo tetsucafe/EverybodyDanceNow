48f307aa50a11cba019bd09cf2f00a22cea1f5a7
tetsucafe <72336987+tetsucafe@users.noreply.github.com>
2021-08-02 20:48:08 +0900

    Update filename_bulkchange.py

--- data_prep/filename_bulkchange.py
+++ data_prep/filename_bulkchange.py
@@ -1,7 +1,7 @@
 import glob
 import os
 
-path = '/EDN/data/target_frames/'
+path = input('frames path: ')
 files = glob.glob(path + '*.png')
 for f in files:
   os.rename(f, f.replace('_rendered', ''))
